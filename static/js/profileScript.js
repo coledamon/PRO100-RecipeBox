5a95a12707e603b9a5a527939aedc250a698bf77
Cole Damon <coledamon2001@gmail.com>
2020-08-19 11:57:06 -0600

    Update profileScript.js

--- static/js/profileScript.js
+++ static/js/profileScript.js
@@ -1,7 +1,5 @@
 $(document).ready(() =>{
-    console.log("hi");
     $("#profPic").on("change", "#file", function(event) {
-        console.log("yo");
         console.log(event);
         console.log(URL.createObjectURL(event.target.files[0]))
         $("#pfp").attr("src", URL.createObjectURL(event.target.files[0]));
